c7e8ee765e735c4223a9ba1aae2e317a1e3495ff
txphan <txphan@cs.olemiss.edu>
2024-04-19 09:12:36 -0500

    first commit

---
+++ MeTube/src/data.js
@@ -0,0 +1,13 @@
+export const API_KEY = 'AIzaSyDatd8hUdK1cTJWBzMYHL6qT-k-GJXiiJI';
+
+export const value_converter = (value) => {
+    if(value>=1000000){
+        return Math.floor(value/1000000)+"M";
+    }
+    else if(value>=1000){
+        return Math.floor(value/1000)+"K"; 
+    }
+    else{
+        return value;
+    }
+}
